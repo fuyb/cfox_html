501041826a00244645ba75f6399b72e1019b9531
fuyanbin <been1986@gmail.com>
2019-04-13 12:50:25 +0800

    set touch-screen

--- cfox/src/index.js
+++ cfox/src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Player from './player';
+import './css/ui.css'
 
-ReactDOM.render(<Player />, document.querySelector("#app"));
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            let orig = document.body.className;
+            document.body.className = orig + 'touch-screen';
+        }
+    }
+
+    render() {
+        return (
+            <Player />
+        );
+    }
+}
+
+ReactDOM.render(<Index />, document.querySelector("#app"));
